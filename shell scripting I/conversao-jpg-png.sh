2127fd496149213f671d412b9de5991c7654d37d
Xunhor <juniorcn@hotmail.com>
2023-07-10 23:16:41 -0300

    conversão de imagem - jpg para png

---
+++ shell scripting I/conversao-jpg-png.sh
@@ -0,0 +1,7 @@
+!/bin/bash # interpretador do script
+# baixe primeiro imagemagick
+
+convert ~/Download/imagens-livros/amazon_aws.jpg ~/Download/imagens-livros/amazon_aws.png
+# convertendo imagem jpg para png
+# primeiro o caminho da imagem que queremos converter
+# depois o caminho da imagem que já convertida
